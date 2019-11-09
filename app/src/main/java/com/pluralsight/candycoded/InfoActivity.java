83e8a58d779ee8ffb0c442c4091ce3975b05f9dd
tiffany-ynaffit <tiffmeg.tiff@gmail.com>
2019-11-09 20:25:20 +0300

    "Completed Module"

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -44,4 +44,11 @@ public void createMapIntent(View view)
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view)
+    {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(phoneIntent);
+    }
+
 }
